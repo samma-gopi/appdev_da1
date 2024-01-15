93f149f7e3dc90e12db82bc1db63ba40136d7921
samma-gopi <kamadula.sammagopi2020@vitstudent.ac.in>
2024-01-15 11:28:37 +0530

    added appdev_da1 program

---
+++ appdev_da1.java
@@ -0,0 +1,7 @@
+class appdev_da1{
+    public static void main(String []args){
+    System.out.println("I am samma gopi");
+    System.out.println("my registration number is 20MIC0107");
+    }
+    } 
+    
